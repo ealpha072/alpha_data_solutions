a2930b6e06464e95b1fa0b40a6bf3e51793b96c0
ealpha072 <ealpha072@gmail.com>
2023-06-21 21:02:30 +0300

    User model added

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ import cors from "cors"
 const app = express()
 const PORT = 5000
 
+app.get('/', (req, res)=> {
+    res.send("Hello World!!")
+})
 
 app.listen(PORT, ()=> {
     console.log(`Server running on port ${PORT}`)
