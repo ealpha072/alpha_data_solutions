132132d05eb9edfbcfa66e7bada0b2a34b8694b0
ealpha072 <ealpha072@gmail.com>
2023-06-22 21:56:12 +0300

    Handle form for sign up

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,43 +1,56 @@
+import { useState } from "react";
 import styles from "../styles/Login.module.css"
 import logo from "../assets/logo.png"
 import { Link } from "react-router-dom";
+import useForm from "../Hooks/Formhook";
 
 const Signup = () => {
-  return (
-    <div>
-        <div className={styles.formContainer}>
-            <form action="" className={styles.form}>
-                <div className={styles.logoDiv}>
-                    <img src={logo} alt="" />
-                </div>
 
+    const initialState = {
+        email:"",
+        password:"",
+        confirmPassword:""
+    }
 
-                <div className={styles.formHolder}>
-                    <label htmlFor="email">Email Address</label>
-                    <input type="email" placeholder="Enter email address" />
-                </div>
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
 
-                <div className={styles.formHolder}>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" placeholder="Enter password" />
-                </div>
+    const {formData, handleInputChange, handleSubmit} = useForm(initialState, onSubmit)
 
-                <div className={styles.formHolder}>
-                    <label htmlFor="password">Confirm Password</label>
-                    <input type="password" placeholder="Re-enter password" />
-                </div>
+    return (
+        <div>
+            <div className={styles.formContainer}>
+                <form action="" method="post" className={styles.form} onSubmit={handleSubmit} >
+                    <div className={styles.logoDiv}>
+                        <img src={logo} alt="" />
+                    </div>
+                    <div className={styles.formHolder}>
+                        <label htmlFor="email">Email Address</label>
+                        <input type="email" placeholder="Enter email address" name="email" value={formData.email} onChange={handleInputChange} />
+                    </div>
 
-                <button className={styles.btn}>Sign up</button>
-                <h4>
-                    Dont have an Account ? <Link to={"/login"}>Login</Link>
-                </h4>
-                <h4>
-                    Go <Link to={"/"}>Home</Link>
-                </h4>
-            </form>
+                    <div className={styles.formHolder}>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" placeholder="Enter password" name="password" value={formData.password} onChange={handleInputChange} />
+                    </div>
+
+                    <div className={styles.formHolder}>
+                        <label htmlFor="password">Confirm Password</label>
+                        <input type="password" placeholder="Re-enter password" name="confirmPassword" value={formData.confirmPassword} onChange={handleInputChange} />
+                    </div>
+
+                    <button className={styles.btn} type="submit">Sign up</button>
+                    <h4>
+                        Dont have an Account ? <Link to={"/login"}>Login</Link>
+                    </h4>
+                    <h4>
+                        Go <Link to={"/"}>Home</Link>
+                    </h4>
+                </form>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default Signup
