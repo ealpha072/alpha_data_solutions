ffd57e57b0e6ccaf633faaf76d66abbf4aace1f6
ealpha072 <ealpha072@gmail.com>
2023-07-12 20:01:20 +0300

    Vercel frontend

--- client/src/services/user.js
+++ client/src/services/user.js
@@ -1,5 +1,6 @@
 import axios from "axios"
-const baseUrl = "http://localhost:5000/user"
+//const baseUrl = "http://localhost:5000/user"
+const baseUrl = "https://alpha-data-solutions-backend-git-main-ealpha072.vercel.app/user"
 
 const signUp = async (data) => {
     const request = axios.post(`${baseUrl}/signup`, data)
